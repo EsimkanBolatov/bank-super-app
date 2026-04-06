8d902b365eaa6ebf46016e94827e6f37678ea6bf
EsimkanBolatov <esi.bolat4@gmail.com>
2026-04-06 14:12:58 +0500

    fix

--- app/main.py
+++ app/main.py
@@ -45,5 +45,5 @@ async def root():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Railway даст PORT=8080
+    port = int(os.environ.get("PORT", 8000))  
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
